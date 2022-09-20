69347cfb5eebe2d68754dec9214965f767664236
Vagner B.C <vagner.bcastro@gmail.com>
2022-09-20 16:57:24 -0300

    Add primevue

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
 import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
 
+import 'primevue/resources/themes/tailwind-light/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css '
+
 const app = createApp(App)
 
+app.use(PrimeVue)
 app.use(createPinia())
 app.use(router)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from 'primevue/button'
+import Dialog from 'primevue/dialog'
+import { ref } from 'vue'
+
+const display = ref<boolean>(false)
+</script>
 
 <template>
   <main>Home</main>
+
+  <Button
+    label="Open dialog"
+    icon="pi pi-check"
+    iconPos="right"
+    @click="display = !display"
+  ></Button>
+
+  <Dialog header="Header" footer="Footer" v-model:visible="display">
+    Content
+  </Dialog>
 </template>

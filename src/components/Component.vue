a10928c42ec2ce66cbf4ffd725c59c3671bc4a68
Vagner B.C <vagner.bcastro@gmail.com>
2022-09-20 13:41:15 -0300

    Remove unnecessary files

---
+++ src/components/Component.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import IconTooling from './icons/IconTooling.vue'
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <main>
+    {{ msg }}
+    <div><IconTooling /></div>
+  </main>
+</template>
+
+<style scoped></style>
